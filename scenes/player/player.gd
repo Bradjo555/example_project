661ab3f385d434365342bf6bc5e5be52f616c3dc
Bradjo555 <72301431+Bradjo555@users.noreply.github.com>
2025-03-25 18:58:59 +0100

    player now plays attack animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,17 +3,29 @@ extends CharacterBody2D
 var speed := 200.0
 var gravity := 800.0 
 var jump_force := -300.0  
+var can_move := true  
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
+
+	if not can_move:
+		velocity.x = 0
+		move_and_slide()
+		return
+	
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_force
+
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		can_move = false
+		animated_sprite.play("attack_1")
+		velocity.x = 0 
+		return 
 	
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed
 		animated_sprite.flip_h = direction < 0  
@@ -23,3 +35,7 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.play("idle")  
 	
 	move_and_slide()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "attack_1":
+		can_move = true  
